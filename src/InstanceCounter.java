5ca646289a79bb2adbbe2fa410a3d50ab41ac943
Lam-Felipe Lee <felipe.lee.garcia@gmail.com>
2018-05-14 20:45:03 -0500

    Add class based on "Modifier Types" chapter

---
+++ src/InstanceCounter.java
@@ -0,0 +1,26 @@
+public class InstanceCounter {
+
+    private static int numInstances = 0;
+
+    InstanceCounter() {
+        InstanceCounter.addInstance();
+    }
+
+    protected static int getCount() {
+        return numInstances;
+    }
+
+    private static void addInstance() {
+        numInstances++;
+    }
+
+    public static void main(String[] args) {
+        System.out.println("Starting with " + InstanceCounter.getCount() + "instances.");
+
+        for (int i = 0; i < 500; i++) {
+            new InstanceCounter();
+        }
+
+        System.out.println("Created " + InstanceCounter.getCount() + " instances.");
+    }
+}
